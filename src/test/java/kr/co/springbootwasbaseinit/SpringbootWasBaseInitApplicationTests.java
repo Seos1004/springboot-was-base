ceffe9740060840f7cf0136851dcde55145108be
Seos1004 <save7412@naver.com>
2024-07-05 17:10:23 +0900

    init

---
+++ src/test/java/kr/co/springbootwasbaseinit/SpringbootWasBaseInitApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.co.springbootwasbaseinit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootWasBaseInitApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
