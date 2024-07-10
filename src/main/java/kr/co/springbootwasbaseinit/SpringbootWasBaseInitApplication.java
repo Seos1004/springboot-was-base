54f9653bd023a1503e9abf3dd006c79929be118a
Seos1004 <save7412@naver.com>
2024-07-10 10:42:37 +0900

    docker compose 사용 추가

--- src/main/java/kr/co/springbootwasbaseinit/SpringbootWasBaseInitApplication.java
+++ src/main/java/kr/co/springbootwasbaseinit/SpringbootWasBaseInitApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringbootWasBaseInitApplication {
+public class  SpringbootWasBaseInitApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SpringbootWasBaseInitApplication.class, args);
